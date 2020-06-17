d0c0378486a9008181e998b5632a2d7a89cebd08
Alok kothiyal <alokothiyal007@gmail.com>
2020-06-17 16:20:58 +0530

    added user authentication/sign in

--- sign_up.js
+++ sign_up.js
@@ -1,11 +1,17 @@
+var btn_1 =document.getElementById('btn1')
+btn_1.addEventListener('click',validate_user)
+
 var btn =document.getElementById('btn')
 btn.addEventListener('click',add_user)
 
-var details =localStorage.getItem('user_details') || []
+
 
 function add_user(){
 
     event.preventDefault()
+
+    var details =JSON.parse(localStorage.getItem('user_details')) || []
+
     var obj={}
     var username = document.getElementById('username').value
     var password = document.getElementById('password').value
@@ -14,7 +20,28 @@ function add_user(){
     obj['pwd'] =password
     
     details.push(obj)
-    console.log(obj)
-    
     localStorage.setItem('user_details',JSON.stringify(details))
+
+}
+
+function validate_user(){
+
+    event.preventDefault()
+    var uname = document.getElementById('uname').value
+    var pwd = document.getElementById('pwd').value
+
+    var details = JSON.parse(localStorage.getItem('user_details')) || []
+
+    for(var i=0;i<details.length;i++){
+       if(uname==details[i].user){
+           if(pwd==details[i].pwd){
+               var obj={}
+               obj['user']=uname
+               obj['pwd']=pwd
+               localStorage.setItem('current_user',JSON.stringify(obj))
+               window.location.href = "balance.html";
+               break
+           }
+       }
+    }
 }
